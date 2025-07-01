fd66699daddc357c18f24b08dd631323c3b01ba7
elshehry <elshehry2000@gmail.com>
2025-07-01 20:59:16 +0300

    Displaying holidays

---
+++ src/main/java/com/spring/school/controller/HolidayController.java
@@ -0,0 +1,35 @@
+package com.spring.school.controller;
+
+import com.spring.school.model.Holiday;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HolidayController {
+
+    @RequestMapping("/holidays")
+    public String displayHolidaysPage(Model model) {
+        List<Holiday> holidays = Arrays.asList(
+                new Holiday(" Jan 1 ", "New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ", "Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ", "Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ", "Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ", "Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ", "Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ", "Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ", "Veterans Day", Holiday.Type.FEDERAL)
+        );
+
+        Holiday.Type[] types = Holiday.Type.values();
+        for (Holiday.Type type : types) {
+            model.addAttribute(type.toString(),
+                    holidays.stream().filter(holiday -> holiday.getType().equals(type)).toList());
+        }
+
+        return "holidays";
+    }
+}
