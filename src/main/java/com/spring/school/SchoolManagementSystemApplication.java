56dc0f6c75d498b8bdb2c0880763269d93f6e132
elshehry <elshehry2000@gmail.com>
2025-07-01 13:09:13 +0300

    Home page design

--- src/main/java/com/spring/school/SchoolManagementSystemApplication.java
+++ src/main/java/com/spring/school/SchoolManagementSystemApplication.java
@@ -1,13 +1,13 @@
-package com.learn.spring.university;
+package com.spring.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversityManagementSystemApplication {
+public class SchoolManagementSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UniversityManagementSystemApplication.class, args);
+        SpringApplication.run(SchoolManagementSystemApplication.class, args);
     }
 
 }
