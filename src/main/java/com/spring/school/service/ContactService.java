caa18a3c338ecefa0444fa20995573e9bf5f139f
elshehry <elshehry2000@gmail.com>
2025-07-13 20:40:40 +0300

    Add request scope to ContactService for improved message handling

--- src/main/java/com/spring/school/service/ContactService.java
+++ src/main/java/com/spring/school/service/ContactService.java
@@ -3,9 +3,11 @@ package com.spring.school.service;
 import com.spring.school.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Slf4j
 @Service
+@RequestScope
 public class ContactService {
 
     public boolean saveMessageDetails(Contact contact) {
